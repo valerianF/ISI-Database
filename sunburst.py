660294bfb03dd049cc9adb9f8b03fdc371dbb9fc
valerianF <valersker@DESKTOP-SBP76AR.localdomain>
2021-01-08 12:40:44 +0100

    automatize initial values computation

--- sunburst.py
+++ sunburst.py
@@ -69,7 +69,6 @@ class appObj:
              "Pre-existing<br>Materials", "Feedback<br>Generated", "Auto-Generative", "Infrasounds",
              "Noise<br>Cancellation", "Site's Acoustics<br>Involved", "No<br>Lights", "Spotlights", "Dynamic",
             "Expressive", "Informational", "Didactic", "Therapeutic"]
-            self.values = [1819, 232, 193, 323, 195, 194, 297, 175, 210, 286, 108]
             self.subs = ["SD_Mat", "LS_Dyn"]
             
         elif self.name == 'System Design':
@@ -77,7 +76,6 @@ class appObj:
               "TS_Bio", "TS_Con", "TS_Det", "TS_Env", "SP_Num", "SP_Hea", "SP_Pnt", "SP_Cnt", "SP_Dir", "SG_Obj"]
             self.parents = ["", "SyD", "SyD", "SyD", "TS", "TS", "TS", "TS", "TS", "TS", "TS", "TS", "TS", "SP",
              "SP", "SP", "SP", "SP", "SG"]
-            self.values = [987, 225, 541, 221, 7, 15, 5, 53, 67, 3, 27, 24, 8, 132, 18, 138, 110, 143, 44]
             self.subs = ["TS_Ele", "TS_Mec", "TS_Ide", "TS_Mic", "TS_Ima", "TS_Bio", "TS_Con", "TS_Det", 
             "TS_Env", "SP_Num", "SP_Hea", "SP_Pnt", "SP_Cnt", "SP_Dir", "SG_Obj"]
             self.labels = ["System<br>Design", "Type of<br>Input Device", "Spatialization", "Sound<br>Generation",
@@ -100,26 +98,45 @@ class appObj:
         elif self.name == 'Interaction':
             self.IDs = ["IN", "IA", "IDof", "ODof", "FT", "MC", "IT"]
             self.parents = ["", "IN", "IN", "IN", "IN", "IN", "IN"]
-            self.values =  [1351, 227, 190, 191, 316, 183, 244]
-            self.subs = ["rien"]
+            self.subs = []
             self.labels = ["Interaction", "Inter-Actors", "Input Degrees<br>Of Freedom", "Output Degrees<br>Of Freedom",
               "Feedback<br>Type", "Musical<br>Control", "Interaction<br>Type", "Many", "Few", "One Actor", "None",
               "One Degree", "Two Degrees", "Three or<br>More Degrees", "One ", "Two", "Three or<br>More", "Visual", "Haptic", "Sonic",
               "Heat", "Taste", "Smell", "Process", "Note-Level", "Timbral", "Global<br>Activity", "Network", "Embodied",
               "Visitor's<br>Motion", "Visitor's<br>Sounds", "Eyes'<br>Movements", "Facial<br>Expression", "Brain<br>Activity",
               "Natural<br>Elements"]       
-            
-        self.len = len(self.IDs)
-        self.parentslabels = self.labels[:len(self.IDs)]
-        self.df = pd.DataFrame(dict(
-                    ids = self.IDs, 
-                    parents = self.parents,
-                    values = self.values, 
-                    labels = self.labels[:self.len]
-                    ))
-       
-        
+
+        # Field sunburst is made from different data
         if self.name != 'Field':
+
+            # Initialize values
+            self.values = np.zeros(len(self.IDs))
+            for col in self.data:
+                if len(self.subs) != 0:
+                    for ID in self.IDs[:-len(self.subs)]: 
+                        if ID in col:
+                            self.values[0] += self.data[col].sum()
+                            self.values[self.IDs.index(ID)] += self.data[col].sum()
+                    for ID in self.IDs[len(self.IDs)-len(self.subs):]:
+                        if ID in col:
+                            self.values[self.IDs.index(ID)] += self.data[col].sum()                        
+                else: 
+                    for ID in self.IDs: 
+                        if ID in col:
+                            self.values[0] += self.data[col].sum()
+                            self.values[self.IDs.index(ID)] += self.data[col].sum()
+
+            # Initialize sunburst dictionnary
+            self.len = len(self.IDs)
+            self.parentslabels = self.labels[:len(self.IDs)]
+            self.df = pd.DataFrame(dict(
+                        ids = self.IDs, 
+                        parents = self.parents,
+                        values = self.values, 
+                        labels = self.labels[:self.len]
+                        ))
+       
+            # Update dictionnary from the database
             for col in self.data:      
                 for ID in self.IDs:
                     if ID in col:
