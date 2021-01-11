5aeea3d08121054a5a8f8140daaf1753b3759a1e
valerianF <valersker@DESKTOP-SBP76AR.localdomain>
2021-01-11 16:42:07 +0100

    structuring multi-page app

---
+++ apps/glossary.py
@@ -0,0 +1,23 @@
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+# Glossary page layout
+layout = html.Div([
+    html.H5('Glossary.'),
+
+    dcc.Link('Navigate to main Page', href='/'),
+
+    html.P(style={'paddingBottom': '0.5cm'}),  
+
+    html.Details([
+        html.Summary('Term to define'),
+        html.P('caca prout.')
+    ]),
+
+    html.Details([
+        html.Summary('Term to define'),
+        html.P('caca prout.')
+    ])
+
+])
