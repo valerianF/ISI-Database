e9f12bf9a7582252cdc484a64483e8bd834f7ddb
valerianF <valerian.fraisse@gmail.com>
2021-02-15 15:34:10 +0100

    change tab name

--- app.py
+++ app.py
@@ -53,7 +53,11 @@ Note than CSS files in /asset subfolder are automaticaly imported.
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 """ Initiate the dash application """
-app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, 
+    suppress_callback_exceptions=True,
+    external_stylesheets=external_stylesheets,
+    title='ISI Database',
+    update_title='Loading...')
 server = app.server
 
 """ Local functions """
